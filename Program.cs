795d7761657fb0969d357df095901d2f0497e32f
ujin-moon <evgeniy.sedin@internet.ru>
2023-10-05 22:08:04 +0300

    66. Plus one(добавление единицы в конец массива)

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using _66._Plus_One;
+
+Solution solution = new Solution();
+
+// Тест 1: Проверка добавления единицы к числу без переполнения
+int[] test1 = { 1, 2, 3 };
+int[] result1 = solution.PlusOne(test1);
+Console.WriteLine("Результат теста 1: " + string.Join(", ", result1)); // Ожидаемый результат: 1, 2, 4
+
+// Тест 2: Проверка добавления единицы с переполнением
+int[] test2 = { 9, 9, 9 };
+int[] result2 = solution.PlusOne(test2);
+Console.WriteLine("Результат теста 2: " + string.Join(", ", result2)); // Ожидаемый результат: 1, 0, 0, 0
+
+// Тест 3: Проверка добавления единицы к нулю
+int[] test3 = { 0 };
+int[] result3 = solution.PlusOne(test3);
+Console.WriteLine("Результат теста 3: " + string.Join(", ", result3)); // Ожидаемый результат: 1
---
+++ Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _66._Plus_One
+{
+    public class Solution
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            int i = digits.Length - 1;
+            digits[digits.Length - 1]++;
+            while (i > 0)
+            {
+                if ((digits[i] == 10) && (i != 0))
+                {
+                    digits[i] = 0;
+                    digits[i - 1]++;
+                }
+                i--;
+            }
+            if ((digits[i] == 10) && (i == 0))
+            {
+                digits[i] = 0;
+                int[] arr = new int[digits.Length + 1];
+                arr[0] = 1;
+                Array.Copy(digits, 0, arr, 1, digits.Length);
+                return arr;
+            }
+            return digits;
+        }
+    }
+}
